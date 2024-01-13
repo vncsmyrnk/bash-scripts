fddf7829b6cebc4a61bb4abbb231a764052831eb
vncsmyrnk <vmayrink12@gmail.com>
2024-01-13 10:48:24 -0300

    Adding a script and improving README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# bash-scripts
+# Bash Scripts
+
+![Shell Script](https://img.shields.io/badge/shell_script-%23121011.svg?style=for-the-badge&logo=gnu-bash&logoColor=white)
+
+Collection of useful bash scripts.
---
+++ generate-new-ssh-key.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script generates a new ssh key
+# A email parameter is expected
+
+email=$1
+
+if [ -z "$email" ]; then
+  echo "Usage: ./script.sh <email>"
+  exit 1
+fi
+
+ssh-keygen -t rsa -b 4096 -C $email
+cat ~/.ssh/id_rsa.pub
+
+exit 0
